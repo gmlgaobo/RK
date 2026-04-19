8c0441328aa39e80e6d0c2abfe72b5b22c31f5c0
gmlgaobo <gmlgaobo@users.noreply.github.com>
2026-04-20 01:20:15 +0800

    Remove output.type setting since it doesn't exist in this RKNN version

--- src/yolo_inference.cpp
+++ src/yolo_inference.cpp
@@ -206,7 +206,6 @@ std::vector<PoseDetection> YoloPoseInference::detect(const cv::Mat& bgr_img, flo
     rknn_output output;
     memset(&output, 0, sizeof(output));
     output.index = 0;
-    output.type = RKNN_TENSOR_FLOAT32;
     ret = rknn_outputs_get(rknn_ctx_, 1, &output, nullptr);
     if (ret != 0) {
         fprintf(stderr, "rknn_outputs_get failed: %d\n", ret);
